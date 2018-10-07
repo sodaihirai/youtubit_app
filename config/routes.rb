6f526076124bb4f61a8d08430bcf79c24447b336
Sodai Hirai <hiraisoudai@gmail.com>
2018-10-07 17:26:40 +0900

    Finish static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #static_pages
+  root 'static_pages#home'
+  get '/about', 	  to: 'static_pages#about'
+  get '/help', 		  to: 'static_pages#help'
+  get '/contact', 	  to: 'static_pages#contact'
+  #users
+  get '/signup',      to: 'users#new'
+  get '/unsubscribe', to: 'users#unsubscribe'
+  resources :users
 end
