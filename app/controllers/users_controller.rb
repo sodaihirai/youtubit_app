c7fcb38d61d5a944ad87ebb7d4930816cab1de03
Sodai Hirai <hiraisoudai@gmail.com>
2018-10-08 12:25:20 +0900

    Finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:success] = "登録完了"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
   end
 
   def index
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  	end
 end
---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  
+
+  test "invalid signup should reject" do
+  	get signup_path
+  	assert_no_difference 'User.count' do
+	  	post users_path, params: { user: { name: " ",
+	  										email: " ",
+	  										password: "foo",
+	  									    password_confirmation: "bar" } }	
+	end
+	assert_template 'users/new'
+	assert_select 'div.error_explanation'
+  end
+
+  test "valid signup" do
+  	get signup_path
+  	assert_difference 'User.count', 1 do
+  		post users_path, params: { user: { name: "example",
+  											email: "example@example.com",
+  											password: "foobar",
+  											password_confirmation: "foobar" } }
+  	end
+  	assert_not flash.empty?
+  	follow_redirect!
+  	assert_template 'users/show'
+  end
+
+end
